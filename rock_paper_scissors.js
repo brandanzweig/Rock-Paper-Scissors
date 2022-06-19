609df3fa3db8d89008e700f16d38f6f145a33ab4
Brandan Zweig <brandanzweig@gmail.com>
2022-06-19 17:46:13 -0400

    Plays single round of RPC

---
+++ rock_paper_scissors.js
@@ -0,0 +1,38 @@
+function computerPlay() {
+    let choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+let playerSelection = prompt("Choose Rock, Paper, or Scissors");
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == "rock") {
+        if (computerSelection == "rock"){
+            return "Tie!";
+        } else if (computerSelection == "paper") {
+            return "You Lose! Paper beats Rock";
+        } else {
+            return "You win! Rock beats Scissors";
+        }
+    }
+    else if (playerSelection.toLowerCase() == "paper") {
+        if (computerSelection == "paper"){
+            return "Tie!";
+        } else if (computerSelection == "scissors") {
+            return "You Lose! Scissors beats Paper";
+        } else {
+            return "You win! Paper beats Rock";
+        }
+    }
+    else {
+        if (computerSelection == "scissors"){
+            return "It's a tie";
+        } else if (computerSelection == "rock") {
+            return "You Lose! Rock beats Scissors";
+        } else {
+            return "You win! Scissors beats Paper";
+        }
+    }
+}
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
