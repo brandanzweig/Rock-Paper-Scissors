4f5c02453fcd5b97796fdb177a8cefe36ff9c533
Brandan Zweig <brandanzweig@gmail.com>
2022-06-28 13:06:49 -0400

    Git Test

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,6 +1,6 @@
 function computerPlay() {
-    let choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * choices.length)]
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)] // Test
 }
 
 function playRound(playerSelection, computerSelection) {
