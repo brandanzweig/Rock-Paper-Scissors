ccd9eb15385da0ae0206eb2621e8266e90b0b3e6
Brandan Zweig <brandanzweig@gmail.com>
2022-07-15 11:53:42 -0400

    Run computerPlay function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const computerPlay = () => {
-    const arrayOfChoices = ["Rock", "Paper", "Scissors"];
-    const randomNum = [Math.floor(Math.random() * arrayOfChoices.length)]
-    console.log(arrayOfChoices[randomNum]);
+function computerPlay() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomNum = [Math.floor(Math.random() * choices.length)]
+    console.log(choices[randomNum]);
 }
 computerPlay();
