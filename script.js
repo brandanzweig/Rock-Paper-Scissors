5833ef7e8fb0b04ef6f78cd7d465a1b9b608e5b5
Brandan Zweig <brandanzweig@gmail.com>
2022-07-15 10:54:16 -0400

    Add function for computer rps selection

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-console.log("Hello world");
-  
+const computerPlay = () => {
+    const arrayOfChoices = ["Rock", "Paper", "Scissors"];
+    const randomNum = [Math.floor(Math.random() * arrayOfChoices.length)]
+    console.log(arrayOfChoices[randomNum]);
+}
+computerPlay();
